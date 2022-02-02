846f89e52900d543b8605705cbde9c203809881a
Gelin Eguinosa <eguinosa@gmail.com>
2022-02-02 11:07:18 -0500

    Add function to rename files to ASCII encoding.

--- music_to_ascii.py
+++ music_to_ascii.py
@@ -2,9 +2,9 @@
 # 01-Feb-2021
 
 import eyed3
-from unidecode import unidecode
-from os import rename, listdir 
-from os.path import join, isfile, basename
+from unidecode import unidecode_expect_nonascii
+from os import rename, listdir
+from os.path import join, isfile, basename, dirname
 
 
 def mp3s_listdir(directory_path):
@@ -28,10 +28,42 @@ def mp3s_listdir(directory_path):
     return mp3_filenames
 
 
-if __name__ == '__main__':
-    # Testing 'mp3s_lisdir'
-    filespaths = mp3s_listdir("library/apple_music")
-    for filepath in filespaths:
-        print(basename(filepath))
-    
+def filenames_to_ascii(filepaths):
+    """
+    Receive a list of files (by their paths) that may or may not be in ASCII
+    encoding to renames their filenames to their respective representation in
+    ASCII.
 
+    Return a list of the files (by their paths) with their updated filenames,
+    all in ASCII.
+    """
+    # List to store the paths of the files with their names updated to ASCII.
+    final_filepaths = []
+    for filepath in filepaths:
+        # Check if the filename is in ASCII.
+        current_filename = basename(filepath)
+        if current_filename.isascii():
+            # No need to update the name to ASCII.
+            final_filepaths.append(filepath)
+        else:
+            # We have to update the file name to ASCII.
+            new_filename = unidecode_expect_nonascii(current_filename)
+            new_filepath = join(dirname(filepath), new_filename)
+            rename(filepath, new_filepath)
+            final_filepaths.append(new_filepath)
+
+    # Return the updates filepaths, with the filenames now in ASCII.
+    return final_filepaths
+
+
+if __name__ == '__main__':
+    # # Test 'mp3s_lisdir'
+    # filepaths = mp3s_listdir("library/apple_music")
+    # for filepath in filepaths:
+    #     print(basename(filepath))
+
+    # Test 'filenames_to_ascii'
+    filepaths = mp3s_listdir("library/apple_music")
+    new_filepaths = filenames_to_ascii(filepaths)
+    for i in range(0, len(filepaths)):
+        print(f"{i}: [{basename(filepaths[i])}] -> [{basename(new_filepaths[i])}]")
